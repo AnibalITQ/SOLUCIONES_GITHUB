e6004d98e2563ac4a257b3e4cb1b981141130ec2
AnibalITQ <l21140735@queretaro.tecnm.mx>
2024-11-03 00:27:17 -0600

    Equipo 2: Finished

---
+++ script_equipo2.js
@@ -0,0 +1,5 @@
+const textEquipo2 = document.getElementById("textEquipo2");
+
+textEquipo2.addEventListener("dblclick", () => {
+    textEquipo2.textContent = textEquipo2.textContent.toUpperCase();
+});
