bcb5777c2ef83e59a98dbecf7dd6b880bd684356
AnibalITQ <l21140735@queretaro.tecnm.mx>
2024-11-03 00:16:27 -0600

    Equipo 1: conteo de caracteres

---
+++ script_equipo1.js
@@ -0,0 +1,6 @@
+const inputEquipo1 = document.getElementById("inputEquipo1");
+const charCount = document.getElementById("charCount");
+
+inputEquipo1.addEventListener("input", () => {
+    charCount.textContent = `${inputEquipo1.value.length} caracteres`;
+});
