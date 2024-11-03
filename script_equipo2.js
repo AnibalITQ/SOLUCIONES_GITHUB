45709c78f6b9cd2f50e90e32bc7337634d793512
AnibalITQ <l21140735@queretaro.tecnm.mx>
2024-11-03 16:16:12 -0600

    preparing for test

--- script_equipo2.js
+++ script_equipo2.js
@@ -3,3 +3,5 @@ const textEquipo2 = document.getElementById("textEquipo2");
 textEquipo2.addEventListener("dblclick", () => {
     textEquipo2.textContent = textEquipo2.textContent.toUpperCase();
 });
+
+export default textEquipo2
