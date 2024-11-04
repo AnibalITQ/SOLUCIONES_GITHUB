85123405f1eebfe3fec1097d9ba1989d16dabafe
AnibalITQ <l21140735@queretaro.tecnm.mx>
2024-11-03 20:44:26 -0600

    corrections abt null references

--- script_equipo3.js
+++ script_equipo3.js
@@ -1,12 +1,14 @@
-const carouselImages = ["./resources/imagen1.png", "./resources/cr7.png", "./resources/tecnm.png"]; // Cambia las rutas de las imágenes
+const carouselImages = ["./resources/imagen1.png", "./resources/cr7.png", "./resources/tecnm.png"];
 let currentImageIndex = 0;
-const carouselImage = document.getElementById("carouselImage");
 
 function changeImage() {
-    currentImageIndex = (currentImageIndex + 1) % carouselImages.length;
-    carouselImage.src = carouselImages[currentImageIndex];
+    const carouselImage = document.getElementById("carouselImage"); // Move this inside the function
+    if (carouselImage) { // Check if the element exists
+        currentImageIndex = (currentImageIndex + 1) % carouselImages.length;
+        carouselImage.src = carouselImages[currentImageIndex];
+    }
 }
 
 setInterval(changeImage, 3000);
 
-module.exports = {carouselImages,carouselImage,changeImage}
+module.exports = { carouselImages, changeImage };
