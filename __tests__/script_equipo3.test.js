9036b60b5150693b4f32aba37729883c8a4f38b1
AnibalITQ <l21140735@queretaro.tecnm.mx>
2024-11-03 20:38:46 -0600

    another test

--- __tests__/script_equipo3.test.js
+++ __tests__/script_equipo3.test.js
@@ -2,33 +2,29 @@
  * @jest-environment jsdom
  */
 
+document.body.innerHTML = `<img id="carouselImage" src="${carouselImages[0]}" />`;
 const { carouselImages, carouselImage, changeImage } = require('../script_equipo3.js');
 
-describe('Carousel Image Changer', () => {
-    beforeEach(() => {
-        document.body.innerHTML = `<img id="carouselImage" src="${carouselImages[0]}" />`;
-    });
-
     test('debería cambiar a la siguiente imagen en el arreglo', () => {
-        const carouselImage = document.getElementById("carouselImage");
-
+    
         // Imagen inicial debe ser la primera en el arreglo
-        expect(carouselImage.src).toContain(carouselImages[0]);
-
+        expect(carouselImage.src).toContain("imagen1.png");
+    
         // Llamamos a la función para cambiar de imagen
         changeImage();
-
+    
         // Después de cambiar, debería mostrar la segunda imagen
-        expect(carouselImage.src).toContain(carouselImages[1]);
-
+        expect(carouselImage.src).toContain("cr7.png");
+    
         // Llamamos a la función nuevamente para cambiar de imagen
         changeImage();
-
+    
         // Ahora debería mostrar la tercera imagen
-        expect(carouselImage.src).toContain(carouselImages[2]);
-
+        expect(carouselImage.src).toContain("tecnm.png");
+    
         // Llamamos una vez más para verificar que vuelve a la primera imagen
         changeImage();
-        expect(carouselImage.src).toContain(carouselImages[0]);
+        expect(carouselImage.src).toContain("imagen1.png");
     });
-});
+    
+
