f32578ccf160098b9c06b3b9ce70d6c767eaaf33
AnibalITQ <124332136+AnibalITQ@users.noreply.github.com>
2024-11-04 02:48:53 +0000

    Deploying to gh-pages from @ AnibalITQ/SOLUCIONES_GITHUB@445bf2136b0b1aa7903e062da0772c2bf650f768 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/__tests__/script_equipo4.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+document.body.innerHTML = `
+    <div id="contentEquipo4" style="display: none;"></div>
+    <button id="toggleButton"></button>
+`;
+
+const { contentEquipo4, toggleButton } = require("../script_equipo4");
+
+test("Toggles contentEquipo4 visibility on button click", () => {
+    toggleButton.click();
+    expect(contentEquipo4.style.display).toBe("block");
+
+    toggleButton.click();
+    expect(contentEquipo4.style.display).toBe("none");
+});
