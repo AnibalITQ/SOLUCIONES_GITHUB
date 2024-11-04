2c62b2ba0dcdcb363220e8d3c78edfe8036eb93a
AnibalITQ <124332136+AnibalITQ@users.noreply.github.com>
2024-11-04 02:58:11 +0000

    Deploying to gh-pages from @ AnibalITQ/SOLUCIONES_GITHUB@762ae0d6e4571e53c12a621f2026bf1c4bea6fd2 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/__tests__/script_equipo5.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+document.body.innerHTML = `
+    <div id="progressBar" style="width: 0%;"></div>
+`;
+
+const { progressBar } = require("../script_equipo5");
+
+test("Progress bar width changes on scroll", () => {
+    document.documentElement.scrollTop = 50;
+    document.documentElement.scrollHeight = 500;
+    document.documentElement.clientHeight = 400;
+
+    window.dispatchEvent(new Event("scroll"));
+    expect(progressBar.style.width).toBe("50%");
+});
---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/eslint.config.mjs
@@ -0,0 +1,11 @@
+export default [
+    {
+      files: ["**/*.js"], // Adjust as needed for the file types you want to lint
+      rules: {
+        "no-unused-vars": "warn",
+        "no-console": "warn",
+        // Add any other rules you need
+      },
+    },
+  ];
+  
