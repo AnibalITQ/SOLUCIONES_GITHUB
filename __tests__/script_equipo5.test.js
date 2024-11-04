0a750572392d31ff4c592f5ba1891f323c698765
AnibalITQ <l21140735@queretaro.tecnm.mx>
2024-11-03 21:22:23 -0600

    adapted test

--- __tests__/script_equipo5.test.js
+++ __tests__/script_equipo5.test.js
@@ -3,16 +3,22 @@
  */
 
 document.body.innerHTML = `
-    <div id="progressBar" style="width: 0%;"></div>
+    <div id="progressBarContainer" style="width: 100%; background-color: #e0e0e0;">
+        <div id="progressBar" style="width: 0%; height: 20px; background-color: #76c7c0;"></div>
+    </div>
 `;
 
 const { progressBar } = require("../script_equipo5");
 
 test("Progress bar width changes on scroll", () => {
-    document.documentElement.scrollTop = 50;
+    // Set up the scroll properties to achieve 50% scroll position
+    document.documentElement.scrollTop = 250;
     document.documentElement.scrollHeight = 500;
-    document.documentElement.clientHeight = 400;
+    document.documentElement.clientHeight = 250;
 
+    // Dispatch the scroll event
     window.dispatchEvent(new Event("scroll"));
+
+    // Assert the progress bar width
     expect(progressBar.style.width).toBe("50%");
 });
